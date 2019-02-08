a14d3263a5a4e57222cc3f7ab5f6aa0508a7d6eb
ZatsepinSergACCBOX <zatsepin@accbox.info>
2019-02-08 22:25:58 +0200

    add default image

--- resources/views/learn/finish.blade.php
+++ resources/views/learn/finish.blade.php
@@ -4,8 +4,13 @@
 @section('content')
     <div class="col-sm-3 col-lg-3"></div>
     <div class="col-sm-5 col-lg-5" style="border: 1px solid #ddd;margin-top: 40px;border-radius: 5px;padding: 20px 20px 20px 20px;background: aliceblue;">
+        @if(!empty($resultTest['image']))
         <img src="{{URL::asset("storage/".$resultTest['image'])}}"
              class="img-thumbnail">
+        @else
+            <img src="{{URL::asset("default.png")}}"
+                 class="img-thumbnail">
+         @endif
         <p>
             Поздравляем!!!,<b> {{$resultTest['email']}}</b>
         </p>
